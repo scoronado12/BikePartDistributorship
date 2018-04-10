4ef18ed82978cd09e2e2ffc93ee359018b11dbe3
Tripp <Tripp@10.213.151.125>
2018-04-10 15:47:44 -0400

    added TEST message in comment

---
+++ BikePartDistributorshipNet/src/sample/Main.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Main extends Application {
+// TEST
+    /**
+     * Start(Stage primaryStage) - Gives the JavaFX Window certain properties
+     * @param primaryStage - the Window
+     * @throws Exception
+     */
+	//mitchell testing
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("project2gui.fxml"));
+        primaryStage.setTitle("Bicycle Parts Distributorship - by Stefano Coronado");
+        primaryStage.setScene(new Scene(root, 775, 510));
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> System.out.println("Stage is closing")); //added in later on to handle saving and exiting
+    }
+
+    /**Stop() - Overridden method that runs upon exiting
+     *
+     * @throws IOException
+     */
+
+    @Override
+    public void stop() throws IOException {
+
+        System.out.println("Saving Warehouses");
+        Controller.doSaveOfWh();
+        System.out.println("Saving inventory.");
+        Controller.doSaveOfBp();
+        System.out.println("Goodbye!");
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

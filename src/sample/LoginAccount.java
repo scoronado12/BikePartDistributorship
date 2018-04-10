a229aa9703b2734ec2d05017f8c2798d20f92ba4
Stefano Coronado <stefanocorona12@gmail.com>
2018-04-10 16:15:58 -0400

    created whmanager, fixed loginaccount, fixed gitmsgs

--- src/sample/LoginAccount.java
+++ src/sample/LoginAccount.java
@@ -1,10 +1,9 @@
 package sample;
-<<<<<<< HEAD
+
 /**
 =======
 
-/**\
->>>>>>> branch 'master' of https://github.com/scoronado12/BikePartDistributorship
+/* branch 'master' of https://github.com/scoronado12/BikePartDistributorship
  * A simple login account with a username and password, as well as a name and email
  * @author Mitchell Bisson
  *
@@ -14,20 +13,20 @@ public class LoginAccount extends Person {
 	protected String username;
 	protected String password;
 	/**
-	 * Constructor
+	 * LoginAccount Constructor
 	 * @param username ; username for the account
 	 * @param password ; password for the account
 	 * @param firstName ; creates a first name for the person
 	 * @param lastName ; creates a last name for the person
 	 */
 	public LoginAccount(String username, String password, String firstName, String lastName) {
+	    super(firstName, lastName);
 		this.username = username;
 		this.password = password;
-		super(firstName, lastName);
 	}
 	
 	/**
-	 * getUsername
+	 * getUsername()
 	 * @return username
 	 */
 	public String getUsername() {
@@ -52,7 +51,7 @@ public class LoginAccount extends Person {
 	
 	/**
 	 * setPassword
-	 * @param username ; creates a new username
+	 * @param username - creates a new username
 	 */
 	public void setPassword(String password) {
 		this.password = password;
---
+++ src/sample/WhManager.java
@@ -0,0 +1,32 @@
+package sample;
+
+public class WhManager extends LoginAccount{
+
+
+    /**
+     * Warehouse Manager Constructor
+     *
+     * @param username  ; username for the account
+     * @param password  ; password for the account
+     * @param firstName ; creates a first name for the person
+     * @param lastName  ; creates a last name for the person
+     */
+    public WhManager(String username, String password, String firstName, String lastName) {
+        super(username, password, firstName, lastName);
+    }
+
+    public void displayInventory(){
+
+    }
+
+    public void transferInventory(){
+
+    }
+
+    public void updateInventory(){
+
+    }
+
+
+
+}

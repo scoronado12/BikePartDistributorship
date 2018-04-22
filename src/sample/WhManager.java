c94410478a98d401c9b50b92bb40958b355b51b5
Mitchell Bisson <mbisson@mail.umw.edu>
2018-04-22 16:59:55 -0400

    Working on updateInventory

--- src/sample/WhManager.java
+++ src/sample/WhManager.java
@@ -4,8 +4,10 @@ import basic.LoginAccount;
 import basic.Warehouse;
 import basic.fileActions;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WhManager extends LoginAccount {
 
@@ -20,6 +22,7 @@ public class WhManager extends LoginAccount {
      * @param Warehouse; The maintained warehouse of the user
      */
     protected Warehouse wh = new Warehouse();
+	private Scanner read;
 /**
  * WhManager Object
  * @param username ; username for account
@@ -75,13 +78,52 @@ public class WhManager extends LoginAccount {
          */
 
 
-        ArrayList parts = (ArrayList) fileActions.readWhDB(fileName);
+        ArrayList<BikePart> parts = new ArrayList<BikePart>();
+        File file = new File(fileName);
+        read = new Scanner(file);
+        
+        while(read.hasNextLine()) {
+        	try 
+    		{        
+    			read = new Scanner(file);
+    			while (read.hasNextLine()) 
+    			{            
+    				String line = read.nextLine();
+    				String regExp = "\\s*(\\s|,)\\s*";
+    				String[] splits = line.split(regExp);
+    				
+    				String partName = splits[0];
+    				int partNum = Integer.parseInt(splits[1]);
+    				double partPrice = Double.parseDouble(splits[2]);
+    				double salesPrice = Double.parseDouble(splits[3]);
+    				boolean isSale = Boolean.parseBoolean(splits[4]);
+    				int quantity = Integer.parseInt(splits[5]);
+    				
+    				BikePart bp = new BikePart(partName,partNum,partPrice,salesPrice,isSale,quantity);
+    				parts.add(bp);
+    				
+    				
+    				
+    				}
+    			System.out.println("File Read");
+    			}    
+    		catch (FileNotFoundException e) 
+    		{        
+    			System.out.println("File not found");
+    			e.printStackTrace();
+    			}
+    		
+    		}
+        
+        //update the warehouse's array list
+        for(int i = 0; i < wh.)
+        }
         
         
 
 
 
-    }
+
 
     /**
      * toString - prints out the object instance as a String
